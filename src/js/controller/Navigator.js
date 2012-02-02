7febfe9593c135795214689341371cbb3052b8c7
Ivan Mashintsev <mashintsev@gmail.com>
2012-02-02 15:42:50 +0400

    Closes #174
    Запретить менять статусы записи пока идет ее аплоад

--- src/js/controller/Navigator.js
+++ src/js/controller/Navigator.js
@@ -81,15 +81,6 @@ Ext.regController('Navigator', {
                 
                 if(store.findExact('id', view.objectRecord.get('xid')) !== -1)
                     this.controlButtonsVisibilities(view, true);
-                    
-                    var statusBar = view.form.getComponent('statusToolbar');
-                    
-                    if(statusBar) {
-						
-						var segBtn = statusBar.getComponent('processing')
-                        
-                        segBtn.disable();
-                    }
             }
         }
     },
@@ -118,8 +109,6 @@ Ext.regController('Navigator', {
 						var segBtn = statusBar.getComponent('processing'),
 							state = record.get('processing');
 						;
-                        
-                        segBtn.enable()
 						
 						segBtn.getComponent(state).enable();
 						segBtn.setPressed(state, true, false);
