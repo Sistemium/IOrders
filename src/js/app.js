848b561c7e34200a1b5600e879c64b58e460c907
Alexander Levin <alevin.ru@gmail.com>
2013-09-27 11:17:08 +0200

    iOS7 viewport fix

--- src/js/app.js
+++ src/js/app.js
@@ -56,6 +56,18 @@ Ext.regApplication({
 			objectRecord: IOrders.mainMenuRecord
 		}));
 		
+		var iOS = parseFloat(
+			('' + (/CPU.*OS ([0-9_]{1,5})|(CPU like).*AppleWebKit.*Mobile/i.exec(navigator.userAgent) || [0,''])[1])
+			.replace('undefined', '3_2').replace('_', '.').replace('_', '')
+			) || false
+		;
+		
+		if (iOS == 7) IOrders.viewport.on('orientationchange', function(e) {
+			var fixY = 0;
+			if (e.orientation == 'landscape') fixY = 20;
+			this.setPosition({y:fixY});
+		});
+		
 		this.viewport.getActiveItem().loadData();
 	},
 	
