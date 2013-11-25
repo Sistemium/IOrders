c71ee11dde10e0f295e960fccbd674e38d1f84e4
Alexander Levin <alevin.ru@gmail.com>
2013-11-25 11:01:56 +0400

    toDisplayString .00

--- src/js/utils.js
+++ src/js/utils.js
@@ -335,7 +335,7 @@ Ext.apply(Number.prototype, {
 		var nStr  = this.toString() + '';
 		x = nStr.split('.');
 		x1 = x[0];
-		x2 = x.length > 1 ? '.' + x[1] : '';
+		x2 = x.length > 1 ? '.' + x[1] : '.00';
 		var rgx = /(\d+)(\d{3})/;
 		while (rgx.test(x1)) {
 			x1 = x1.replace(rgx, '$1' + ',' + '$2');
